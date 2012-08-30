d86f2fc6dc5d20282e278a85ca8d1b13d3e58681
elff1 <elff1@21cn.com>
2012-08-30 08:11:58 +0800

    initialize, something wrong with tags and comments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '52baaf7d4b575793a48137fb8e57220c5732556e6527758303c0a22c8d12765f3da15dceed7d0180f3dd44d1c98ddcd6dd4d89d46a0b518d86f5537150b06370'
